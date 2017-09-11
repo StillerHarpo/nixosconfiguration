2050ea6fede55fdff630d678883c853247000a23
Floran Engel <floriaengel39@gmail.com>
2017-09-11 20:15:37 +0200

    Control the fans

--- desktopSpecifc.nix
+++ desktopSpecifc.nix
@@ -8,6 +8,13 @@
   
   # luks encryption
   boot.initrd.luks.devices.luksroot.device = "/dev/sdd2";
-
-
+  
+  # fancontrol
+  boot.kernelModules = [ "it87" ];
+  environment.systemPackages = [ pkgs.lm_sensors ];
+  systemd.services.fancontrol = {
+    description = "fancontrol daemon";
+    wantedBy = [ "multi-user.target" ];
+    script = "${pkgs.lm_sensors}/sbin/fancontrol /etc/nixos/fancontrol";
+  };
 }
