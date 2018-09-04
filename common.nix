2632b4d7ae65e95e037eb9ed9214a08b1fa8ced6
Florian Engel <florianengel39@gmail.com>
2018-09-04 11:33:11 +0200

    Add hie-nix binary cache

--- common.nix
+++ common.nix
@@ -182,6 +182,17 @@
     opengl.driSupport32Bit = true;
   };
 
+  nix = {
+    binaryCaches = [
+      "https://cache.nixos.org/"
+      "https://hie-nix.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
+    ];
+    trustedUsers = [ "root" "florian" ];
+  };
+
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "18.09";
 }
