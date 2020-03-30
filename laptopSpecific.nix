3c2b74ac168c85e7fb83ed74f2a14a9878e629b7
Florian Engel <florianengel39@gmail.com>
2020-03-30 14:34:21 +0200

    Mute audio after resume from hibernate

--- laptopSpecific.nix
+++ laptopSpecific.nix
@@ -72,6 +72,7 @@
               ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-2 --off
               ${pkgs.xorg.xrandr}/bin/xrandr --output --eDP-1 --mode 1920x1080
             fi
+            ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ true
           '';
         serviceConfig = {
           Type = "oneshot";
