ed0c72d9082925c7f28e3946f67dd2143e1d3e51
Florian Engel <florianengel39@gmail.com>
2021-05-27 23:00:12 +0200

    Fix linux zsh

--- zshLinux.nix
+++ zshLinux.nix
@@ -3,14 +3,14 @@
   programs.zsh = {
     shellAliases.ls = "ls -lh --color=auto";
     oh-my-zsh.theme = "agnoster";
+    localVariables.PROMPT = ''
+       WORKSPACE=$(wmctrl -d | grep "*" | cut -f1 -d' ')
+       if [ $SHELL != "/run/current-system/sw/bin/zsh" ]
+       then
+         PROMPT='%K{green}%F{black} nix-shell %F{green}%K{black}$(echo "\ue0b0")%k%f'$PROMPT
+       else
+         cd $(grep -e "^''${WORKSPACE} .*" ~/scripts/var/roots | cut -f2 -d' ')
+       fi
+    '';
   };
-  localVariables.PROMPT = ''
-     WORKSPACE=$(wmctrl -d | grep "*" | cut -f1 -d' ')
-     if [ $SHELL != "/run/current-system/sw/bin/zsh" ]
-     then
-       PROMPT='%K{green}%F{black} nix-shell %F{green}%K{black}$(echo "\ue0b0")%k%f'$PROMPT
-     else
-       cd $(grep -e "^''${WORKSPACE} .*" ~/scripts/var/roots | cut -f2 -d' ')
-     fi
-  '';
 }
