698c46a4b450d5d5a07fe866970760fd59474db6
Florian Engel <florianengel39@gmail.com>
2022-03-22 07:11:25 +0100

    Do osr after scan

--- scripts/Scan.hs
+++ scripts/Scan.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -15,6 +14,7 @@ import Control.Monad.Trans.Class
 import Data.IORef
 import qualified Control.Foldl as Fold
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Data.Semigroup(Max(..))
 import Data.Foldable(traverse_)
 import Data.Time.Format
@@ -22,26 +22,33 @@ import System.IO(hSetBuffering, BufferMode(..), stdin)
 import Control.Monad.Catch
 import Control.Monad.Extra
 import Text.Read (readMaybe)
+import System.IO.Extra (withTempDir, withTempFile)
 
 data Env = Env { counter :: IORef Int
-               , scanner :: Text }
+               , scanner :: Text
+               , tesseractConf :: Text }
 
 data CustomError =
   FileExistError FilePath
+  | DirNotExistsError FilePath
   | EncodingError
-  | PnmsNotDevidableByError Int
+  | NotDevidableByError Int
   | NoScanner
 
 instance Show CustomError where
   show = \case
-     FileExistError filePath -> T.unpack $ format ("Error file"%fp%"exist") filePath
+     FileExistError filePath -> T.unpack $ format ("Error file "%fp%" exist") filePath
+     DirNotExistsError filePath -> T.unpack $ format ("Directory "%fp%" does not exist") filePath
      EncodingError -> "encodig failed"
-     PnmsNotDevidableByError n -> "internal error: number of pnsm should be devideable by " <> show n
+     NotDevidableByError n -> "internal error: number of input files should be devideable by " <> show n
      NoScanner -> "Couldn't find scanner"
 
 instance Exception CustomError
 
 type MyShell a = ReaderT Env Shell a
+
+_PDFS_DIR :: FilePath
+_PDFS_DIR = "/home/florian/Dokumente/paperlessInput/"
 
 getCounter :: MyShell Int
 getCounter = asks counter >>= liftIO . readIORef
@@ -49,14 +56,42 @@ getCounter = asks counter >>= liftIO . readIORef
 incCounter :: MyShell ()
 incCounter = asks counter >>= liftIO . flip modifyIORef' (+ 1)
 
-getPnms :: Shell FilePath
-getPnms = mfilter ((== Just "pnm") . snd . splitExtension) (ls ".")
+getTifs :: Shell FilePath
+getTifs = mfilter ((== Just "tif") . snd . splitExtension) (ls ".")
 
-pnmNumMatch :: Pattern Int
-pnmNumMatch =  "./out" *> decimal <* ".pnm"
+getPdfs :: MyShell Text
+getPdfs = lift getTifs >>= runTesseract
 
-getPnmsSorted :: MonadIO m => m [FilePath]
-getPnmsSorted = sortOn ((match pnmNumMatch <$>) . toText) getPnms
+fileNumMatch :: Pattern Text -> Pattern Int
+fileNumMatch = ("./out" *> decimal <*)
+
+tifNumMatch :: Pattern Int
+tifNumMatch =  fileNumMatch ".tif"
+
+pdfNumMatch :: Pattern Int
+pdfNumMatch =  fileNumMatch ".pdf"
+
+delFile :: MonadIO io => FilePath -> io ()
+delFile file = do
+  printf ("delting " % fp % "\n") file
+  rm file
+
+runTesseract :: FilePath -> MyShell Text
+runTesseract inputFile =
+  case (toText inputFile, toText (basename inputFile)) of
+    (Right inputFileT, Right outputBaseT) -> do
+      Env {..} <- ask
+      printf ("running tesseract on " % fp % "\n") inputFile
+      procs "tesseract" [inputFileT, outputBaseT, "-l", "deu", tesseractConf] mempty
+      delFile inputFile
+      pure (outputBaseT <> ".pdf")
+    _ -> throwM EncodingError
+
+liftFunReader :: Monad m => (m a -> m b) -> ReaderT s m a -> ReaderT s m b
+liftFunReader fun r =  ask >>= lift . fun . runReaderT r
+
+getPdfsSorted :: MyShell [Text]
+getPdfsSorted = liftFunReader (sortOn (match pdfNumMatch)) getPdfs
 
 fileExistsError :: FilePath -> MyShell ()
 fileExistsError file = do
@@ -81,39 +116,37 @@ checkFile file = do
   then fileExistsError file
   else pure ()
 
-convert :: [FilePath] -> MyShell ()
+convert :: [Text] -> MyShell ()
 convert inputs = do
   fnT <- getFileName
   let fn = fromText fnT
-  case traverse toText inputs of
-        Right inputTs -> do
-           checkFile fn
-           printf ("Writing "%fp%"\n") fn
-           procs "convert" (inputTs <> ["-quality", "100", fnT]) mempty
-           incCounter
-        _ -> throwM EncodingError
+  checkFile fn
+  printf ("Writing "%fp%" from files "%s%"\n") fn (T.intercalate ", " inputs)
+  procs "pdfunite" (inputs <> [fnT]) mempty
+  mv fn (_PDFS_DIR <> fn)
+  incCounter
 
 convertPictures :: MyShell ()
 convertPictures = do
-  pnm <- lift getPnms
-  convert [pnm]
-  rm pnm
+  pdf <- getPdfs
+  convert [pdf]
+  delFile (fromText pdf)
 
 convertMultiblePictures :: MyShell ()
 convertMultiblePictures = do
-  pnms <- getPnmsSorted
-  convert pnms
-  traverse_ rm pnms
+  pdfs <- getPdfsSorted
+  convert pdfs
+  traverse_ (delFile . fromText) pdfs
 
 convertNPictures :: Int -> MyShell ()
 convertNPictures i = do
-  pnmsSorted <- getPnmsSorted
-  let everyN pnms
-        | length pnms >= i = (take i pnms :) <$> everyN (drop i pnms)
-        | null pnms = pure []
-        | otherwise = throwM (PnmsNotDevidableByError i)
-  everyN pnmsSorted >>= traverse_ convert
-  traverse_ rm pnmsSorted
+  pdfsSorted <- getPdfsSorted
+  let everyN pdfs
+        | length pdfs >= i = (take i pdfs :) <$> everyN (drop i pdfs)
+        | null pdfs = pure []
+        | otherwise = throwM (NotDevidableByError i)
+  everyN pdfsSorted >>= traverse_ convert
+  traverse_ (delFile . fromText) pdfsSorted
 
 sourceToNumPicture :: ScanSource -> Int -> Int
 sourceToNumPicture source =
@@ -128,8 +161,8 @@ page scanSource i = do
 
 scanMultible :: ScanSource -> MyShell ()
 scanMultible scanSource = do
-  i <- getMaxFile pnmNumMatch
-  scanimage scanSource ["--batch-start", T.pack (show i)]
+  start <- getMaxFile tifNumMatch
+  scanimage scanSource start
   echo "Press y or Y to add pages, any other char will finish scan"
   whenM (flip elem ['y', 'Y'] <$> liftIO getChar) $
     scanMultible scanSource
@@ -141,27 +174,38 @@ data ScanSource =
 isExitKey :: Char -> Bool
 isExitKey = (`elem` ['q', 'Q', 'X', 'x'])
 
-scanimage :: ScanSource -> [Text] -> MyShell ()
-scanimage source extraOptions = do
+isExitKeyStr :: String -> Bool
+isExitKeyStr = \case
+  [] -> False
+  (c:cs) -> null cs && isExitKey c
+
+mainMenuKeysHint :: MonadIO io => io ()
+mainMenuKeysHint = echo "press q,Q,X,x to enter main menu"
+
+scanimage :: ScanSource -> Int -> MyShell ()
+scanimage source start = do
   device <- asks scanner
-  catch
-    ( sh $
-        inproc
-          "scanimage"
-          ( ["--batch", "-d", device, "--mode", "Color", "--page-height", "0"]
-              <> scanSourceOption source
-              <> extraOptions
-          )
-          mempty
-    )
-    ( \(_ :: SomeException) -> do
-        echo "press q,Q,X,x to quit press any other key to try again"
-        liftIO getChar
-          >>= ( \case
-                  (isExitKey -> True) -> mainMenu
-                  _ -> scanimage source extraOptions
-              )
-    )
+  (exitCode, stdOut) <-
+    procStrict
+      "scanimage"
+      ( ["--batch", "-d", device, "--mode", "Color", "--page-height", "0", "--format=tiff"]
+          <> scanSourceOption source
+          <> ["--batch-start", T.pack (show start)]
+      )
+      mempty
+  liftIO $ T.putStrLn stdOut
+  case exitCode of
+    ExitSuccess -> pure ()
+    (ExitFailure _) -> do
+      newStart <- getMaxFile tifNumMatch
+      echo "Exception occured"
+      mainMenuKeysHint
+      echo "Any other key will try again"
+      liftIO getChar
+        >>= ( \case
+                (isExitKey -> True) -> mainMenu
+                _ -> scanimage source newStart
+            )
 
 scanSourceOption :: ScanSource -> [Text]
 scanSourceOption scanSource =
@@ -172,11 +216,14 @@ scanSourceOption scanSource =
 withPageNumbers :: ScanSource -> MyShell ()
 withPageNumbers scanSource = do
   echo "Enter how many pages each document has (duplex pages count as one)"
+  mainMenuKeysHint
   liftIO getLine
-    >>= maybe
-      (echo "Invalid number" >> mainMenu)
-      (page scanSource)
-      . readMaybe
+    >>= \case
+          (readMaybe -> Just n) -> page scanSource n
+          (isExitKeyStr -> True) -> mainMenu
+          _ -> do
+             echo "Invalid number"
+             withPageNumbers scanSource
 
 mainMenu :: MyShell ()
 mainMenu = do
@@ -184,7 +231,7 @@ mainMenu = do
   echo "Press d for single duplex page"
   echo "Press x,X,q or Q to exit the script"
   let invalid = do
-        echo "You have entered an invallid selection!"
+        echo "You have entered an invalid selection!"
         echo "Please try again!"
         echo ""
         echo "Press any key to continue..."
@@ -201,7 +248,7 @@ getMaxFile :: MonadIO m => Pattern Int -> m Int
 getMaxFile fileParser =
    fold
      ((match fileParser <$>)
-      . toText <$> ls ".")
+      . toText <$> ls _PDFS_DIR)
      (Fold.foldMap
        (\case
            Right [i] -> Max $ i + 1
@@ -211,6 +258,7 @@ getMaxFile fileParser =
 main :: IO ()
 main = do
  hSetBuffering stdin NoBuffering
+ unlessM (testdir _PDFS_DIR) (throwM $ DirNotExistsError _PDFS_DIR)
  t <- today
  initialCounter <- getMaxFile ("./input_" *> decimal <* text ("_" <> t <> ".pdf"))
  counter <- liftIO $ newIORef initialCounter
@@ -220,9 +268,14 @@ main = do
       & grep (contains "epjitsu")
       & sed (chars1 *> "epjitsu" <> (T.init <$> ends "'") <* chars1))
      Fold.head
- sh $ case mayScanner of
+ case mayScanner of
    Just (lineToText -> scanner) ->
-     (getPnms >>= rm) <|> runReaderT mainMenu (Env{..})
+     withTempDir $ \dir ->
+       withTempFile $ \tesseractConfStr -> do
+         echo "writing tesseract config"
+         writeFile tesseractConfStr "tessedit_create_pdf 1"
+         cd $ fromText (T.pack dir)
+         pwd >>= printf ("current directory is now: "%fp%"\n")
+         let tesseractConf = T.pack tesseractConfStr
+         sh $ runReaderT mainMenu (Env{..})
    Nothing -> throwM NoScanner
- `finally`
-  sh (getPnms >>= rm)
