f4521df70192fdaf3607d5ff11a437a276ba37b3
Florian Engel <florianengel39@gmail.com>
2022-09-07 18:54:18 +0200

    Remove dnscrypt

--- linux/configuration.nix
+++ linux/configuration.nix
@@ -100,24 +100,6 @@
     useUserPackages = true;
   };
 
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      ipv6_servers = true;
-      require_dnssec = true;
-
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        # can't use default because it is in nix store. Should be fixed upstream
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-      };
-
-    };
-  };
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "21.05";
 }
