95ed1f433df6c1597760fc20b149a00ed65acd1a
Florian Engel <florianengel39@gmail.com>
2022-09-19 19:19:03 +0200

    Make work container a module

--- linux/thinkpad/configuration.nix
+++ linux/thinkpad/configuration.nix
@@ -186,18 +186,6 @@
       };
     };
     rtkit.enable = true;
-    polkit = {
-      enable = true;
-      extraConfig = ''
-        polkit.addRule(function(action, subject) {
-            if (action.id == "org.freedesktop.machine1.shell") {
-                if (subject.isInGroup("wheel")) {
-                    return polkit.Result.YES;
-                }
-            }
-        });
-      '';
-    };
   };
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
--- linux/thinkpad/work-container.nix
+++ linux/thinkpad/work-container.nix
@@ -1,128 +1,173 @@
 { config, lib, pkgs, home-manager-flake, ... }:
+
+with lib;
+
 let
-    hostCfg = config;
-    userName = "florian";
-    userUid = hostCfg.users.users."${userName}".uid;
-    xauth = "/tmp/work_xauth";
-    outerPkgs = pkgs;
+  outerPkgs = pkgs;
+  cfg = config.workContainer;
 in {
 
-  networking = {
-    nat = {
-      enable = true;
-      internalInterfaces = ["ve-+"];
-      externalInterface = "+";
+  options = {
+
+    workContainer = {
+      enable = mkEnableOption (lib.mdDoc "Work container");
     };
-    networkmanager = {
-      unmanaged = [ "interface-name:ve-*" ];
+
+    userName = mkOption {
+      type = types.str;
+      default = "florian";
+      example = "florian";
+      description = lib.mdDoc "User in the container";
+    };
+
+    xauth = mkOption {
+      type = types.path;
+      default = "/tmp/work_xauth";
+      example = "/tmp/work_xauth";
+      description = lib.mdDoc "xauth file to access host xserver";
+    };
+
+    uid = mkOption {
+      type = types.int;
+      default = config.users.users."${userName}".uid;
+      example = "0";
+      description = lib.mdDoc "xauth file to access host xserver";
     };
   };
 
-  services.xserver.displayManager.sessionCommands = ''
-    xauth nextract - "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f "${xauth}" nmerge -
-  '';
+  config = mkIf cfg.enable {
 
-  containers.work = {
-    # TODO and bind for file transfer
-    bindMounts = {
-      x11Display = rec {
-        hostPath = "/tmp/.X11-unix";
-        mountPoint = hostPath;
-        isReadOnly = true;
+    networking = {
+      nat = {
+        enable = true;
+        internalInterfaces = ["ve-+"];
+        externalInterface = "+";
       };
-      xAuthority = rec {
-        hostPath = xauth;
-        mountPoint = hostPath;
-        isReadOnly = true;
+      networkmanager = {
+        unmanaged = [ "interface-name:ve-*" ];
       };
     };
 
-    enableTun = true;
-    privateNetwork = true;
-    hostAddress = "192.168.10.1";
-    localAddress = "192.168.10.2";
+    security.polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+            if (action.id == "org.freedesktop.machine1.shell" || action.id == "org.freedesktop.machine1.start") {
+                if (subject.isInGroup("wheel")) {
+                    return polkit.Result.YES;
+                }
+            }
+        });
+      '';
+    };
 
-    config = {
-      imports = [ home-manager-flake ];
-      users.users."${userName}" = {
-        extraGroups = lib.mkForce [];
-        isNormalUser = true;
-        shell = pkgs.zsh;
+    services.xserver.displayManager.sessionCommands = ''
+    xauth nextract - "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f "${xauth}" nmerge -
+    machinectl start work
+  '';
+
+    containers.work = {
+      # TODO and bind for file transfer
+      bindMounts = {
+        x11Display = rec {
+          hostPath = "/tmp/.X11-unix";
+          mountPoint = hostPath;
+          isReadOnly = true;
+        };
+        xAuthority = rec {
+          hostPath = xauth;
+          mountPoint = hostPath;
+          isReadOnly = true;
+        };
       };
 
-      systemd.services.fix-nix-dirs = let
-        profileDir = "/nix/var/nix/profiles/per-user/${userName}";
-        gcrootsDir = "/nix/var/nix/gcroots/per-user/${userName}";
-      in {
-        script = ''
+      enableTun = true;
+      privateNetwork = true;
+      hostAddress = "192.168.10.1";
+      localAddress = "192.168.10.2";
+
+      config = {
+        imports = [ home-manager-flake ];
+        users.users."${userName}" = {
+          extraGroups = lib.mkForce [];
+          isNormalUser = true;
+          shell = pkgs.zsh;
+        };
+
+        systemd.services.fix-nix-dirs = let
+          profileDir = "/nix/var/nix/profiles/per-user/${userName}";
+          gcrootsDir = "/nix/var/nix/gcroots/per-user/${userName}";
+        in {
+          script = ''
           #!${pkgs.stdenv.shell}
           set -euo pipefail
 
           mkdir -p ${profileDir} ${gcrootsDir}
           chown ${userName}:root ${profileDir} ${gcrootsDir}
         '';
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "oneshot";
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+          };
         };
-      };
-      hardware.opengl = {
-        enable = true;
-        extraPackages = hostCfg.hardware.opengl.extraPackages;
-      };
+        hardware.opengl = {
+          enable = true;
+          extraPackages = hostCfg.hardware.opengl.extraPackages;
+        };
 
-      environment.systemPackages = with pkgs; [
-        git
-        firefox
-        remmina
-      ];
+        environment.systemPackages = with pkgs; [
+          git
+          firefox
+          remmina
+        ];
 
-      environment.etc."office.ovpn".source = ./office.ovpn;
+        environment.etc."office.ovpn".source = ./office.ovpn;
 
-      services.openvpn.servers.officeVPN = {
-        config = '' config /etc/office.ovpn '';
-        autoStart = true;
-      };
+        services.openvpn.servers.officeVPN = {
+          config = '' config /etc/office.ovpn '';
+          autoStart = true;
+        };
 
-#      networking.defaultGateway.address = "192.168.10.1";
-      networking.nameservers = [ "8.8.8.8" ];
+        #      networking.defaultGateway.address = "192.168.10.1";
+        networking.nameservers = [ "8.8.8.8" ];
 
-      home-manager = {
-        users."${userName}" = {
-          imports = [
-            ../../zsh.nix
-            home/firefox.nix
-          ];
-          programs.zsh = {
-            enable = true;
-            oh-my-zsh.theme = "agnoster";
-            shellAliases = {
-              ll = "ls -a";
-              "." ="cd ..";
-              ".." = "cd ../..";
-              "..." = "cd ../../..";
-              "...." = "cd ../../../..";
-            };
-            enableAutosuggestions = true;
-            oh-my-zsh = {
+        home-manager = {
+          users."${cfg.userName}" = {
+            imports = [
+              ../../zsh.nix
+              home/firefox.nix
+            ];
+            programs.zsh = {
               enable = true;
-              plugins = [ "vi-mode" "per-directory-history" ];
+              oh-my-zsh.theme = "agnoster";
+              shellAliases = {
+                ll = "ls -a";
+                "." ="cd ..";
+                ".." = "cd ../..";
+                "..." = "cd ../../..";
+                "...." = "cd ../../../..";
+              };
+              enableAutosuggestions = true;
+              oh-my-zsh = {
+                enable = true;
+                plugins = [ "vi-mode" "per-directory-history" ];
+              };
             };
+            programs.ssh.enable = true;
+            home.sessionVariables = {
+              SHELL = "zsh";
+              DISPLAY = ":0";
+              XDG_RUNTIME_DIR = "/run/user/${toString cfg.uid}";
+              XAUTHORITY  = cfg.xauth;
+            };
+            home.stateVersion = "22.05";
+            nixpkgs.overlays = outerPkgs.overlays;
           };
-          programs.ssh.enable = true;
-          home.sessionVariables = {
-            SHELL = "zsh";
-            DISPLAY = ":0";
-            XDG_RUNTIME_DIR = "/run/user/${toString userUid}";
-            XAUTHORITY  = xauth;
-          };
-          home.stateVersion = "22.05";
-          nixpkgs.overlays = outerPkgs.overlays;
         };
-      };
 
-      system.stateVersion = "22.05";
-      nixpkgs.overlays = outerPkgs.overlays;
+        system.stateVersion = "22.05";
+        nixpkgs.overlays = outerPkgs.overlays;
+      };
     };
   };
 }
