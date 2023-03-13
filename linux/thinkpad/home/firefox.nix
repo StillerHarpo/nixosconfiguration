45532eeaf22cb77143a286b4293bb78e90d72140
Florian Engel <florianengel39@gmail.com>
2023-03-13 21:01:26 +0100

    Remove duplicated firefox

--- linux/thinkpad/home/firefox.nix
+++ linux/thinkpad/home/firefox.nix
@@ -43,52 +43,60 @@
         ffreszoom
         german-dictonary
       ];
-    profiles.default.settings = {
-      # colors
-      "widget.content.allow-gtk-dark-theme" = true;
-      "browser.display.use_system_colors" = true;
+    profiles = let
+      commonSettings = {
+        # colors
+        "widget.content.allow-gtk-dark-theme" = true;
+        "browser.display.use_system_colors" = true;
 
-      "browser.urlbar.placeholderName" = "DuckDuckGo";
+        "browser.urlbar.placeholderName" = "DuckDuckGo";
 
-      # Enable HTTPS-Only Mode
-      "dom.security.https_only_mode" = true;
-      "dom.security.https_only_mode_ever_enabled" = true;
+        # Enable HTTPS-Only Mode
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode_ever_enabled" = true;
 
-      # Privacy settings
-      "privacy.donottrackheader.enabled" = true;
-      "privacy.trackingprotection.enabled" = true;
-      "privacy.trackingprotection.socialtracking.enabled" = true;
-      "privacy.partition.network_state.ocsp_cache" = true;
+        # Privacy settings
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+        "privacy.partition.network_state.ocsp_cache" = true;
 
-      # Disable all sorts of telemetry
-      "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-      "browser.newtabpage.activity-stream.telemetry" = false;
-      "browser.ping-centre.telemetry" = false;
-      "toolkit.telemetry.archive.enabled" = false;
-      "toolkit.telemetry.bhrPing.enabled" = false;
-      "toolkit.telemetry.enabled" = false;
-      "toolkit.telemetry.firstShutdownPing.enabled" = false;
-      "toolkit.telemetry.hybridContent.enabled" = false;
-      "toolkit.telemetry.newProfilePing.enabled" = false;
-      "toolkit.telemetry.reportingpolicy.firstRun" = false;
-      "toolkit.telemetry.shutdownPingSender.enabled" = false;
-      "toolkit.telemetry.unified" = false;
-      "toolkit.telemetry.updatePing.enabled" = false;
+        # Disable all sorts of telemetry
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.hybridContent.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.reportingpolicy.firstRun" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
 
-      # As well as Firefox 'experiments'
-      "experiments.activeExperiment" = false;
-      "experiments.enabled" = false;
-      "experiments.supported" = false;
-      "network.allow-experiments" = false;
+        # As well as Firefox 'experiments'
+        "experiments.activeExperiment" = false;
+        "experiments.enabled" = false;
+        "experiments.supported" = false;
+        "network.allow-experiments" = false;
 
-      # Disable Pocket Integration
-      "browser.newtabpage.activity-stream.section.highlights.includePocket" =
-        false;
-      "extensions.pocket.enabled" = false;
-      "extensions.pocket.api" = "";
-      "extensions.pocket.oAuthConsumerKey" = "";
-      "extensions.pocket.showHome" = false;
-      "extensions.pocket.site" = "";
+        # Disable Pocket Integration
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" =
+          false;
+        "extensions.pocket.enabled" = false;
+        "extensions.pocket.api" = "";
+        "extensions.pocket.oAuthConsumerKey" = "";
+        "extensions.pocket.showHome" = false;
+        "extensions.pocket.site" = "";
+      };
+    in {
+      default.settings = commonSettings;
+      work = {
+        id = 1;
+        settings = commonSettings;
+      };
     };
   };
 
