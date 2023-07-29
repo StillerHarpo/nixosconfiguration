a4d95028539a20382f648688313f80612263279f
Florian Engel <engelflorian@posteo.de>
2023-07-29 16:48:56 +0200

    Emacs: Disable bell

--- config.el
+++ config.el
@@ -3,7 +3,7 @@
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (menu-bar-mode -1)           ; Disable menubar
-(setq visible-bell -1)
+(setq ring-bell-function 'ignore)
 
 (require 'use-package)
 
