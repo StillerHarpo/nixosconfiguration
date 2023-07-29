8f2faf0218c93bd788be57a42aa5d5c1e6527f59
Florian Engel <engelflorian@posteo.de>
2023-07-29 09:14:36 +0200

    Emacs: Fix eslint formatting

--- config.el
+++ config.el
@@ -429,15 +429,18 @@
   :custom
   (tide-server-max-response-length 204800)
   :init
-  (defun eslint-format ()
+  (defun my/eslint-format ()
     "runs eslint on the current file, falls back on tide-format"
-    (unless
-	(= (call-process "npx" nil nil nil "eslint" "--quiet" "--fix" (buffer-file-name (current-buffer))) 0)
-      (tide-format-before-save)))
+    (interactive)
+    (envrc-propagate-environment (lambda ()
+      (when
+      	(= (call-process "npx" nil nil nil "eslint" "--quiet" "--fix" (buffer-file-name (current-buffer))) 127)
+        (message "command npx not found")
+        (tide-format-before-save)))))
   :hook ((typescript-ts-mode . tide-setup)
          (tsx-ts-mode . tide-setup)
          (typescript-ts-mode . tide-hl-identifier-mode)
-         (before-save . eslint-format))
+         (before-save . my/eslint-format))
   :config
   (evil-add-command-properties #'tide-goto-line-reference :jump t)
   (evil-add-command-properties #'tide-jump-to-definition :jump t)
